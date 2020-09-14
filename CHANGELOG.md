66c9e685280d062f1b7e319a99b366502fc9164d
houthacker <houthacker@houthacker.io>
2020-09-14 20:56:02 +0200

    Add cache for failed uploads

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change log
 
+## 1.2 - 2020-09-14
+### Added
+* Cache for failed PVOutput uploads
+
 ## 1.1 - 2016-10-17
 ### Added
 * App will try to reconnect when inverter connection is lost (instead of stopping)
--- pvoutput.py
+++ pvoutput.py
@@ -12,43 +12,69 @@ import sys
 
 logger = logging.getLogger(__name__)
 
+
 class System:
     """Provides methods for direct uploading to PVOutput for set system."""
     
     def __init__(self, api_key, system_id):
         self.api_key = api_key
         self.system_id = system_id
+        self.cached_types = ['add_output', 'add_status']
+        self.cache = dict()
+
+        for t in self.cached_types:
+            self.cache[t] = []
     
     def add_output(self, data):
         """Add end of day output information. Data should be a dictionary with
         parameters as described here:
         http://pvoutput.org/help.html#api-addoutput ."""
         url = "http://pvoutput.org/service/r2/addoutput.jsp"
-        self.__make_request(url, data)
+        self.__make_request(url, data, 'add_output')
     
     def add_status(self, data):
         """Add live output data. Data should contain the parameters as described
         here: http://pvoutput.org/help.html#api-addstatus ."""
         url = "http://pvoutput.org/service/r2/addstatus.jsp"
-        self.__make_request(url, data)
+        self.__make_request(url, data, 'add_status')
     
     # Could add methods like 'get_status'
 
-    def __make_request(self, url, data):
+    def __make_request(self, url, data, request_type):
         logger.debug('Making request: %s, %s', url, data)
         data = urllib.parse.urlencode(data).encode('ascii')
         req = urllib.request.Request(url, data)
         req.add_header('X-Pvoutput-Apikey', self.api_key)
         req.add_header('X-Pvoutput-SystemId', self.system_id)
+
+        if self.__send_request(req, request_type):
+            self.__retry_failed_request()
+
+    def __send_request(self, req, request_type):
         try:
             f = urllib.request.urlopen(req)
         except urllib.error.HTTPError as e:
             logger.error('Upload failed: %s', e.read().decode())
+            self.cache[request_type].append(req)
         except urllib.error.URLError as e:
             logger.error('Upload failed: %s', e)
+            self.cache[request_type].append(req)
         else:
             with f:
                 logger.debug('Response: %s', f.read().decode())
+            return True
+
+        return False
+
+    def __retry_failed_request(self):
+        """Tries to send at most two previously failed requests.
+        No more requests are retried to prevent exhaustion the PVOutput API rate limit"""
+        for cached_type in self.cached_types:
+            if len(self.cache[cached_type]) > 0:
+                req = self.cache[cached_type].pop()
+
+                if not self.__send_request(req, cached_type):
+                    self.cache[cached_type].append(req)
 
     def __str__(self):
         return self.system_id.__str__()
